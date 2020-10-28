a0ffad35b91191e2545d87a69244ab667b09b454
Brujo Benavides <elbrujohalcon@gmail.com>
2020-10-28 09:49:14 +0100

    [rebar3_format-0.6.0] Pin rebar3_format to its latest version

--- include/dinerl_types.hrl
+++ include/dinerl_types.hrl
@@ -12,23 +12,23 @@
 -type key() :: [key_element()].
 -type jsonf() :: any().
 -type clientarguments() :: {{access_key_id(), secret_access_key()}, zone(), rfcdate()}.
--type method() :: batch_get_item |
-                  get_item |
-                  put_item |
-                  delete_item |
-                  update_item |
-                  create_table |
-                  list_tables |
-                  describe_table |
-                  update_table |
-                  delete_table |
-                  q |
-                  scan |
-                  query_item_20111205 |
-                  query_item_20120810.
--type result() :: {ok, any()} |
-                  {error, string(), string()} |
-                  {error, term(), timeout | string()}.
+-type method() ::
+    batch_get_item |
+    get_item |
+    put_item |
+    delete_item |
+    update_item |
+    create_table |
+    list_tables |
+    describe_table |
+    update_table |
+    delete_table |
+    q |
+    scan |
+    query_item_20111205 |
+    query_item_20120810.
+-type result() ::
+    {ok, any()} | {error, string(), string()} | {error, term(), timeout | string()}.
 -type header() :: {string() | atom(), string()}.
 -type headers() :: [header()].
 -type attribute() :: {string(), {s | n, string()}} | {string(), {ns | ss, [string()]}}.
