3c052c00d19b7b8b00f87ab6a8e6eeb63b8f83a1
Brujo Benavides <elbrujohalcon@gmail.com>
2020-11-09 08:58:45 +0100

    [rebar3_format-0.7.0] Pin rebar3_format to its latest version

--- src/dmochijson2.erl
+++ src/dmochijson2.erl
@@ -51,11 +51,15 @@
 -define(INC_COL(S),
         S#decoder{offset = 1 + S#decoder.offset, column = 1 + S#decoder.column}).
 -define(INC_LINE(S),
-        S#decoder{offset = 1 + S#decoder.offset, column = 1, line = 1 + S#decoder.line}).
+        S#decoder{offset = 1 + S#decoder.offset,
+                  column = 1,
+                  line = 1 + S#decoder.line}).
 -define(INC_CHAR(S, C),
         case C of
             $\n ->
-                S#decoder{column = 1, line = 1 + S#decoder.line, offset = 1 + S#decoder.offset};
+                S#decoder{column = 1,
+                          line = 1 + S#decoder.line,
+                          offset = 1 + S#decoder.offset};
             _ ->
                 S#decoder{column = 1 + S#decoder.column, offset = 1 + S#decoder.offset}
         end).
