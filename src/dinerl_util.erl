b12eb39889b997ff7fcb4daee74115b71a10586a
Miriam Pena <miriam.pena@adroll.com>
2020-12-02 07:20:32 -0800

    Instrument calls to lhttpc (#35)
    * Instrument calls to lhttpc
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updae
    
    * update format

---
+++ src/dinerl_util.erl
@@ -0,0 +1,66 @@
+-module(dinerl_util).
+
+-export([noop/2, time_call/2, time_call/3, increment/1, increment/2]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @equiv time_call(Metric, Fun, 1.0)
+-spec time_call(term(), fun(() -> Result)) -> Result.
+time_call(Metric, Fun) ->
+    time_call(Metric, Fun, 1.0).
+
+%% @doc Evaluates Fun() and (with a probability of Ratio) reports its evaluation
+%%      time as a histogram metric called Metric.
+-spec time_call(term(), fun(() -> Result), number()) -> Result.
+time_call(Metric, Fun, 1.0) ->
+    Start = erlang:system_time(microsecond),
+    Result = Fun(),
+    End = erlang:system_time(microsecond),
+    Diff = End - Start,
+    histogram(Metric, Diff),
+    Result;
+time_call(Metric, Fun, Ratio) when is_number(Ratio) ->
+    Start = erlang:system_time(microsecond),
+    Result = Fun(),
+    case rand:uniform() =< Ratio of
+        true ->
+            End = erlang:system_time(microsecond),
+            Diff = End - Start,
+            histogram(Metric, Diff);
+        false ->
+            ok
+    end,
+    Result.
+
+histogram(Metric, Value) ->
+    {StatMod, StatFun} = histogram_stat_callback(),
+    erlang:apply(StatMod, StatFun, [Metric, Value]).
+
+increment(Metric) ->
+    increment(Metric, 1).
+
+increment(Metric, Value) ->
+    {StatMod, StatFun} = increment_stat_callback(),
+    erlang:apply(StatMod, StatFun, [Metric, Value]).
+
+noop(_Key, _Value) ->
+    ok.
+
+%%%===================================================================
+%%% Internal Functions
+%%%===================================================================
+histogram_stat_callback() ->
+    get_env(histogram_stat_callback).
+
+increment_stat_callback() ->
+    get_env(increment_stat_callback).
+
+get_env(Key) ->
+    case application:get_env(dinerl, Key) of
+        {ok, Value} ->
+            Value;
+        undefined ->
+            exit({undefined_configuration, Key})
+    end.
--- src/dynamodb.erl
+++ src/dynamodb.erl
@@ -57,28 +57,64 @@ call(Credentials, Zone, Target, ISODate, Body, Timeout) ->
                         host => Host,
                         region => region(Zone)},
                       Body),
-    submit("http://" ++ Host ++ "/",
-           [{"content-type", "application/x-amz-json-1.0"} | Headers],
-           Body,
-           Timeout).
+    submit(Host, [{"content-type", "application/x-amz-json-1.0"} | Headers], Body, Timeout).
 
 call(Credentials, Zone, Target, RFCDate, Body) ->
     call(Credentials, Zone, Target, RFCDate, Body, 1000).
 
 -spec submit(endpoint(), headers(), iolist(), integer()) -> result().
-submit(Endpoint, Headers, Body, Timeout) ->
+submit(Host, Headers, Body, Timeout) when is_list(Host) ->
     Opts = [{max_connections, 10000}],
-    case lhttpc:request(Endpoint, "POST", Headers, Body, Timeout, Opts) of
+    Endpoint = "http://" ++ Host ++ "/",
+    dinerl_util:increment([dinerl, dynamodb, call, {endpoint, list_to_atom(Host)}]),
+    F = fun() -> lhttpc:request(Endpoint, "POST", Headers, Body, Timeout, Opts) end,
+    case dinerl_util:time_call([dinerl, dynamodb, call, time, list_to_atom(Host)], F) of
         {ok, {{200, _}, _Headers, Response}} ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, ok}]),
             {ok, Response};
         {ok, {{400, Code}, _Headers, ErrorString}} ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, '400'}]),
             {error, Code, ErrorString};
         {ok, {{413, Code}, _Headers, ErrorString}} ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, '413'}]),
             {error, Code, ErrorString};
         {ok, {{500, Code}, _Headers, ErrorString}} ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, '500'}]),
             {error, Code, ErrorString};
         {error, Reason} ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, error}]),
             {error, unknown, Reason};
         Other ->
+            dinerl_util:increment([dinerl,
+                                   dynamodb,
+                                   call,
+                                   result,
+                                   {host, list_to_atom(Host)},
+                                   {result, unknown}]),
             {error, response, Other}
     end.
